e947b30f1e3f1beced088a48de5a71e9098c753a
mariiazhivonitko <mariia.zhivonitko@gmail.com>
2023-02-27 22:05:58 +0200

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -8,9 +8,8 @@ const API_KEY = "136fc3acefd2479fb671a2d84bb2ebd1";
 
 
 function App() {
-  const [error, setError] = useState(null)
-  const [isLoaded, setisLoaded] = useState(false)
-  const [items, setItems] = useState([])
+  
+  //const [items, setItems] = useState([])
   
   const [summary, setSummary] = useState('')
   const [title, setTitle] = useState('')
@@ -23,9 +22,8 @@ function App() {
   
     axios.get(address)
       .then((response) => {
-        setError(null)
-        setisLoaded(true)
-        setItems(response.data.recipes)
+      
+        //setItems(response.data.recipes)
         setSummary(response.data.recipes[0].summary);
         setTitle(response.data.recipes[0].title);
         setImage(response.data.recipes[0].image);
@@ -36,7 +34,7 @@ function App() {
   }, [])
 
   console.log(summary);
-  console.log(items);
+ // console.log(items);
 
   return (
     <div>
